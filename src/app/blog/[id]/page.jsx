906050e5e0db900ec9a907cd49b1f455ace79c27
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-08-15 20:39:13 -0700

    page styling and fetches for blog

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,32 +1,46 @@
 import React from 'react'
 import styles from './page.module.css';
 import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
-const BlogPost = () => {
+async function getData(id) {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+    cache: 'no-store'
+  });
+
+  if (!res.ok) {
+    return notFound();
+  }
+
+  return res.json();
+}
+
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            {data.title}
           </h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem provident consequatur maxime veritatis optio eius numquam earum odit atque nisi unde ad, cupiditate at excepturi odio, rem tempore amet similique.
+            {data.desc}
           </p>
           <div className={styles.author}>
             <Image
-              src="https://images.pexels.com/2345245"
+              src={data.img}
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>John Doe</span>
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imgContainer}>
           <Image
-            src="https://images.pexels.com/23455234"
+            src={data.img}
             alt=""
             fill={true}
             className={styles.img}
@@ -35,7 +49,7 @@ const BlogPost = () => {
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim eveniet molestiae impedit dolore reiciendis perferendis est natus minima atque incidunt pariatur vitae unde eius accusamus qui adipisci inventore perspiciatis, quo obcaecati velit corporis. Deleniti, voluptatibus. Quae fuga ab incidunt fugiat consequuntur nostrum explicabo? Itaque deleniti reprehenderit quae nostrum dicta reiciendis dignissimos eaque eum earum rerum atque nisi expedita maiores est iste, quia repudiandae amet quasi impedit. Obcaecati sint blanditiis tempore aut dolor quos provident consectetur earum voluptatum. Maxime nulla ea sit, rerum minus, animi quidem perferendis deleniti, accusantium deserunt odio cupiditate! Est illo ipsa at maxime veritatis consequatur doloribus, eaque distinctio recusandae temporibus. Porro dolore eaque similique deserunt quibusdam voluptas error suscipit rerum, fuga iusto eveniet est labore laboriosam magni, voluptates veniam impedit reprehenderit quod doloribus! Velit quas vitae, eius itaque perspiciatis autem fugiat dolores sapiente rem tenetur doloremque hic architecto a quidem. Accusamus, magni nobis! Pariatur odit cupiditate ducimus repellat distinctio accusamus quis dicta. Sequi quod consequuntur quae! Odit unde delectus pariatur alias placeat qui, magni eos aspernatur reprehenderit necessitatibus perspiciatis reiciendis sit natus magnam sapiente neque quo commodi quae odio laboriosam dolorem incidunt ratione mollitia. Doloribus impedit sint aperiam! Omnis nesciunt ea, vero aut tempora culpa aperiam amet?
+          {data.content}
         </p>
       </div>
     </div>
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,74 +2,45 @@ import React from 'react';
 import styles from './page.module.css';
 import Button from '@/components/Button/Button';
 import Image from 'next/image';
+import { items } from './data';
+import { notFound } from 'next/navigation';
+
+const getData = (cat) => {
+  const data = items[cat];
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+}
+
 
 const Category = ({ params }) => {
-  console.log(params);
+  const data = getData(params.category);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>
         {params.category}
       </h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url="#" text="See More" />
+      {data.map(item => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button url="#" text="See More" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              fill
+              src={item.image}
+              alt=""
+            />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="https://images.pexels.com/213465234523"
-            alt=""
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="https://images.pexels.com/213465234523"
-            alt=""
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="https://images.pexels.com/213465234523"
-            alt=""
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="https://images.pexels.com/213465234523"
-            alt=""
-          />
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
