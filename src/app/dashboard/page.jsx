8ba4b275e820cc88937f9bc251047d3f301cbedc
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-08-15 23:01:05 -0700

    auth w/ nextAuth

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,14 +3,16 @@
 import React from 'react'
 import styles from './page.module.css';
 import useSWR from 'swr';
+import { useSession } from 'next-auth/react';
 
 
 const Dashboard = () => {
+  const session = useSession();
+  console.log(session);
+
   const fetcher = (...args) => fetch(...args).then(res => res.json());
 
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher);
-
-  console.log(data);
 
   return (
     <div className={styles.container}>
